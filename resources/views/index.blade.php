888ac629972c4d7a2b56737f33f63de83aa50692
ferdy1807 <ferdy.perdana@yahoo.com>
2021-11-21 14:19:48 +0700

    sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,6 @@
 
 @section('title', 'Page Title')
 
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
-@endsection
-
 @section('content')
     <h1>HALAMAN UTAMA</h1>
 @endsection
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,14 +1,15 @@
 <nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-        data-accordion="false">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
 with font-awesome or any other icon font library -->
-        <li class="nav-item menu-open">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-table"></i>
-                <p>Dashboard
-                </p>
+        <li class="nav-item">
+        <a href="/" class="nav-link active">
+            <i class="nav-icon fas fa-table"></i>
+            <p>Dashboard
+            </p>
             </a>
+        </li>
+        <li class="nav-item menu-open">
             <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-angle-double-down"></i>
                 <p>Tabel Data
