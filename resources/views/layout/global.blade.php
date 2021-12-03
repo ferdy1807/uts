5170647486b00c6b15bf61dfc186167962db99d8
ferdy1807 <ferdy.perdana@yahoo.com>
2021-12-03 17:42:58 +0700

    footbar

--- resources/views/layout/global.blade.php
+++ resources/views/layout/global.blade.php
@@ -71,7 +71,7 @@
             <a href="/" class="brand-link">
                 <img src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">Sistem Kepegawaian</span>
             </a>
 
             <!-- Sidebar -->
